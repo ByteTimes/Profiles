bc141302df265955b4cac05ebb290ff885f9b184
SavileLee <SavileLee@users.noreply.github.com>
2020-12-26 00:22:19 +0800

    FilterRules

--- FilterRules/X/Scripts/YouTube.js
+++ FilterRules/X/Scripts/YouTube.js
@@ -1,15 +1,22 @@
 /*
-[Script]
-http-request ^https://[\s\S]*\.googlevideo\.com/.*&(oad|ctier) script-path=https://Choler.github.io/Surge/Script/YouTube.js
+YouTube ADS Remove
 
-[MITM]
-hostname = *.googlevideo.com
+Surge4.0:
+[Script]
+# 如果您是 YouTube Premium，请不要开启此模块。
+youtube_ad = type=http-request,pattern=^https?:\/\/.+?\.googlevideo\.com\/.+&(oad|ctier)=(?!A),script-path=https://raw.githubusercontent.com/SavileLee/Profiles/Rules/FilterRules/X/Scripts/YouTube.js
+
+QX 1.0.0:
+^https?:\/\/.+?\.googlevideo\.com\/.+&(oad|ctier)=(?!A) url script-request-header https://raw.githubusercontent.com/SavileLee/Profiles/Rules/FilterRules/X/Scripts/YouTube.js
+
+Surge & QX Mitm = *.googlevideo.com
 */
 
-var data = {
-  body: "{}",
-  headers: {
-    "Content-Type": "multipart/byteranges"
-  }
-};
-$done({response: data});
+if ($request.url.indexOf("&oad") != -1) {
+  $done({ response: {body: ""} });
+} else if ($request.url.indexOf("&ctier") != -1) {
+  let url = $request.url.replace(/ctier=[A-Z]/, "ctier=A");
+  $done({ response: { status: 302, headers: { Location: url } } });
+} else {
+  $done({})
+}
